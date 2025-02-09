1dae0de4b862867b48c437720f74f5a770c0246f
unknown <jackson.li@mail.utoronto.ca>
2025-02-09 18:45:44 -0500

    construct and connect database

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import mysql.connector
 app = Flask(__name__)
 CORS(app)  # 解决跨域问题
 
 # 计算未来财富的函数
 def calculate_future_wealth(salary, budget, return_rate):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Lbjloves120418",
+        database="IMS",
+    )
+    cursor = conn.cursor()
+
     monthly_investment = float(salary) - float(budget)
     future_wealth = monthly_investment * ((1 + return_rate) ** 12)  # 简单的年增长计算
     return future_wealth
